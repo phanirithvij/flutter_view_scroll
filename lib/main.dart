4858ffa5d0d950030335603637f79365d241778b
phanirithvij <phanirithvij2000@gmail.com>
2019-12-18 15:08:33 +0530

    data tranfer is working perfectly

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(FlutterView());
@@ -23,17 +25,29 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  static const String _channel = 'increment';
+  static const String _pong = 'pong';
+  static const String _emptyMessage = '';
+  static const BasicMessageChannel<String> platform =
+      BasicMessageChannel<String>(_channel, StringCodec());
+
   int _counter = 0;
 
   @override
   void initState() {
     super.initState();
+    platform.setMessageHandler(_handlePlatformIncrement);
   }
 
-  void _handleIncrement() async {
+  Future<String> _handlePlatformIncrement(String message) async {
     setState(() {
       _counter++;
     });
+    return _emptyMessage;
+  }
+
+  void _sendFlutterIncrement() {
+    platform.send(_pong);
   }
 
   @override
@@ -46,7 +60,7 @@ class _MyHomePageState extends State<MyHomePage> {
           Expanded(
             child: Center(
               child: Text(
-                  'Button tapped $_counter time${_counter == 1 ? '' : 's'}.',
+                  'Platform button tapped $_counter time${_counter == 1 ? '' : 's'}.',
                   style: const TextStyle(fontSize: 17.0)),
             ),
           ),
@@ -62,7 +76,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _handleIncrement,
+        onPressed: _sendFlutterIncrement,
         child: const Icon(Icons.add),
       ),
     );
--- lib/main.o.dart
+++ lib/main.o.dart
@@ -1,6 +1,4 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 void main() {
   runApp(FlutterView());
@@ -25,29 +23,17 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  static const String _channel = 'increment';
-  static const String _pong = 'pong';
-  static const String _emptyMessage = '';
-  static const BasicMessageChannel<String> platform =
-      BasicMessageChannel<String>(_channel, StringCodec());
-
   int _counter = 0;
 
   @override
   void initState() {
     super.initState();
-    platform.setMessageHandler(_handlePlatformIncrement);
   }
 
-  Future<String> _handlePlatformIncrement(String message) async {
+  void _handleIncrement() async {
     setState(() {
       _counter++;
     });
-    return _emptyMessage;
-  }
-
-  void _sendFlutterIncrement() {
-    platform.send(_pong);
   }
 
   @override
@@ -60,7 +46,7 @@ class _MyHomePageState extends State<MyHomePage> {
           Expanded(
             child: Center(
               child: Text(
-                  'Platform button tapped $_counter time${_counter == 1 ? '' : 's'}.',
+                  'Button tapped $_counter time${_counter == 1 ? '' : 's'}.',
                   style: const TextStyle(fontSize: 17.0)),
             ),
           ),
@@ -76,7 +62,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _sendFlutterIncrement,
+        onPressed: _handleIncrement,
         child: const Icon(Icons.add),
       ),
     );
